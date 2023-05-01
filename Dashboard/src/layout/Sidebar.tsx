298ce47fb6c5b1b5104402ea591a315f116b0002
mashadservice <safa.mobi.380@gmail.com>
2023-05-01 11:43:10 +0330

    init project

---
+++ Dashboard/src/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+import {Box} from "@mui/material";
+
+const Sidebar = () => {
+    return(
+        <Box bgcolor='aqua' >sidebar</Box>
+    )
+}
+
+export default  Sidebar
---
+++ Dashboard/src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./sidebar/sidebarSlice";
+
+const store = configureStore({
+  reducer: {
+    sidebar: sidebarReducer,
+  },
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
