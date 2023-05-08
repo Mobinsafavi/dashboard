c042fd1a03ca222317f62ea51336a193525c312a
safavi <safa.mobi.380@gmail.com>
2023-05-08 18:15:11 +0330

    feat : creating Footer of mainContainer

--- src/layout/mainContainer/Footer.tsx
+++ src/layout/mainContainer/Footer.tsx
@@ -1,9 +1,43 @@
-import { Box } from "@mui/material";
+import { Grid, List, ListItem, Typography } from "@mui/material";
 
 const Footer = () => {
-  return (<Box>
-    hello world!
-  </Box>)
-}
+  const footerList = [
+    { name: " About US", url: "" },
+    { name: "Contact Us", url: "" },
+  ];
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <Typography variant="body1" color="secondary">
+          © 2023, made by Mobin Safavi for a better web.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        xs={6}
+        sx={{ display: "flex", justifyContent: "end" }}
+      >
+        <List
+          component="nav"
+          aria-labelledby="main navigation"
+          sx={{ display: "flex" }}
+        >
+          {footerList.map((item) => (
+            <ListItem key={item.url} disablePadding sx={{ ml: 2 }}>
+              <Typography
+                variant="body1"
+                color="secondary"
+                sx={{ whiteSpace: "nowrap" }}
+              >
+                {item.name}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Footer
+export default Footer;
