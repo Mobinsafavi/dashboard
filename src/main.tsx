78ed08c66c65cbeebb0c5aa3a902caef4295e999
safavi <safa.mobi.380@gmail.com>
2023-05-08 19:38:48 +0330

    feat : working on Routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/css/sass/main.sass'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/css/sass/main.sass";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./pages/CustomError";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
